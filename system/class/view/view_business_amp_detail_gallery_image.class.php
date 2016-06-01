ba44700b4ebe8a966608c25ee2b66d7b9b1d767b
Allen Wu <allenpluto@yahoo.com.cn>
2016-06-01 10:58:52 +1000

    Version 2.1.13i
    use curl instead of file_get_contents to force pre generate default
    image (to get proper width and height) in amp image class

--- system/class/view/view_business_amp_detail_gallery_image.class.php
+++ system/class/view/view_business_amp_detail_gallery_image.class.php
@@ -26,7 +26,16 @@ class view_business_amp_detail_gallery_image extends view_gallery_image
                 else
                 {
                     // try to generate image if not exists
-                    if (file_get_contents(URI_IMAGE . $parameter['image_size'] . '/' . $row_value['image_file']) !== FALSE)
+                    $url = URI_IMAGE . $parameter['image_size'] . '/' . $row_value['image_file'];
+                    $curl = curl_init();
+                    curl_setopt($curl, CURLOPT_URL, $url);
+                    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+                    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+                    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+                    $auth = curl_exec($curl);
+                    curl_close($curl);
+
+                    if ($auth)
                     {
                         if (file_exists(PATH_IMAGE . $parameter['image_size'] . '/' . $row_value['image_file']))
                         {
