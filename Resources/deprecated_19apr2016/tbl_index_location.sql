a33474c889a4d119841b7ca7674930f160cba728
Allen Wu <allenpluto@yahoo.com.cn>
2016-12-22 09:17:17 +1100

    Mobile Release - Remove unrelated files
    Remove testing, research log, backup source files and etc.

--- Resources/deprecated_19apr2016/tbl_index_location.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS `tbl_index_location`;
-CREATE TABLE `tbl_index_location` AS
-SELECT * FROM `top4_main`.`Postcode_Suburb`;
-ALTER TABLE  `tbl_index_location` ENGINE = MYISAM DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-ALTER TABLE `tbl_index_location` ADD PRIMARY KEY(`id`);
-ALTER TABLE `tbl_index_location` ADD FULLTEXT KEY fulltext_location (suburb, region, state);
--- Resources/deprecated_19apr2016/tbl_view_web_page.sql
+++
@@ -1,4 +0,0 @@
-DROP VIEW IF EXISTS `tbl_view_web_page`;
-CREATE VIEW `tbl_view_web_page` AS
-SELECT web_page.id, web_page.friendly_url, web_page.name, web_page.description, web_page.meta_keywords, web_page.page_title, web_page.page_content, web_page.image_id, web_page_image.image_file FROM tbl_entity_web_page web_page
-LEFT JOIN tbl_view_image web_page_image ON web_page.image_id = web_page_image.id
